c2e828f80d417615835b16473427d36e2f67060a
mhartle2 <har09028@byui.edu>
2014-05-15 13:16:37 -0600

    Really merging

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		System.out.println("Hello GitHub");
-=======
-		System.out.println("Hello GitHub World");
 		System.out.println("Testing...");
->>>>>>> test
 	}
 }
